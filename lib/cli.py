60789ecdf1c4dca915ca633f1f1cc04edd6dcd28
Andy Patari <andy.patari@gmail.com>
2023-10-06 19:49:29 -0400

    Define style and main menu templates

--- lib/cli.py
+++ lib/cli.py
@@ -1,28 +1,65 @@
 # lib/cli.py
 
+
+from rich import print as rprint
+from rich.style import Style
+from rich.console import Console
+console = Console()
+
 from helpers import (
     exit_program,
     helper_1
 )
 
+invalid = Style( color='magenta2', bold=True )
 
 def main():
+    
     while True:
         menu()
         choice = input("> ")
-        if choice == "0":
+        if choice == "x":
             exit_program()
-        elif choice == "1":
-            helper_1()
+        elif choice == "c":
+            clubs()
         else:
-            print("Invalid choice")
+            print("")
+            console.print("Invalid entry", style= invalid)
 
 
 def menu():
-    print("Please select an option:")
-    print("0. Exit the program")
-    print("1. Some useful function")
+    print('')
+    console.print("------[blue]Main Menu[/blue]-----------------", style="dark_sea_green bold")
+    print(" ")
+    print("Enter c for clubs menu")
+    print("Enter s for students menu")
+    print("Enter x to exit the program")
+    print('')
 
+
+def clubs():
+    while True:
+        clubs_menu()
+        choice = input("> ")
+        if choice == "x":
+            exit_program()
+        elif choice == "m":
+            main()
+        else:
+            print("")
+            console.print("Invalid entry", style= invalid)
+
+def clubs_menu():
+    print('')
+    console.print("------[purple]Clubs Menu[/purple]-----------------", style="dark_sea_green bold")
+    print("")
+    print("Enter m to return to main menu")
+    print("Enter x to exit the program")
+    print("")
 
 if __name__ == "__main__":
+    print("")
+    console.print("Welcome to the Club Tracker!", style = "dark_orange on white bold")
+    print("")
     main()
+
